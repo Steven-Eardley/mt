5f77766d78c6fb47224221a786848ad0dff3f991
Steven Eardley <s.r.eardley@sms.ed.ac.uk>
2013-02-14 13:54:28 +0000

    difficult...

--- Assignment1/ibm1.py
+++ Assignment1/ibm1.py
@@ -2,6 +2,7 @@
 # Assignment 1 for Machine Translation
 # Steven Eardley s0934142
 
+import re
 from sys import argv
 
 if len(argv) == 3:
@@ -9,6 +10,8 @@ if len(argv) == 3:
     from_file = argv[2]
 else:
     print "\nUse: ibm1.py to_language from_language\nWhere these files are an alligned corpus.\n"
+
+# punctuation tends to be surrounded by spaces.
 
  #initialize t(e|f) uniformly
  #do until convergence
@@ -27,21 +30,45 @@ else:
      #for all e
        #t(e|f) = count(e|f) / total(f)
 
+# Store the alligned sentences in a dictionary: { sentenceno : ([e], [f]) }
+sentences = dict()
+
+# Store translations in a dictionary: { f : [(e , probability)] }
+translations = dict()
+
+# Remove punctuation which is surrounded by spaces, and separate into a list of words
+def list_words(sentence_string):
+    nonpunc_string = re.sub(' \W ', ' ', sentence_string)
+    tokens = nonpunc_string.split(" ")
+    
+    # Get rid of the newline
+    return tokens[:len(tokens)-1]
+
+# Read sentence pairs from two alligned files.
 def readpairs(to_file, from_file):
-        f1 = open(to_file, 'r')
-        f2 = open(from_file, 'r')
+    f1 = open(to_file, 'r')
+    f2 = open(from_file, 'r')
     try:
         to_sentences = f1.readlines()
         from_sentences = f2.readlines()
     finally:
         f1.close()
         f2.close()
-        
     
-    return prob
+    if len(to_sentences) != len(from_sentences):
+        print "Mismatched document lengths!"
+    
+    for i in range(0, len(to_sentences)):
+        sentences[i] = (list_words(to_sentences[i]), list_words(from_sentences[i]))
 
 def train():
+    while not converged:
+        count_ef = 0
+    
     return 0
 
 def translate():
     return 0
+    
+readpairs(to_file, from_file)
+print sentences[1]
